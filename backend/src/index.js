5e50d96ede7d39724f80048143d8c57be1664211
skguravana <146762519+skguravana@users.noreply.github.com>
2025-07-04 21:52:57 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true
 }));
 
