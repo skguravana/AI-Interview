a592d60ad4278de3b42101c143f1521507ec0e70
skguravana <146762519+skguravana@users.noreply.github.com>
2025-07-05 09:28:17 +0530

    Update utils.js

--- backend/src/libs/utils.js
+++ backend/src/libs/utils.js
@@ -12,7 +12,7 @@ export const generateToken = (userId,res) =>{
         maxAge:7*24*60*60*1000,
         httpOnly:true,
         sameSite:"strict",
-        secure: false,
+        secure: true,
     })
     return token;
 }
