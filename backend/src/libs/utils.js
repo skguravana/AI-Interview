07a4aded0ed44af8ca9612282737cf210776d72b
skguravana <146762519+skguravana@users.noreply.github.com>
2025-07-05 09:29:17 +0530

    Update utils.js

--- backend/src/libs/utils.js
+++ backend/src/libs/utils.js
@@ -11,7 +11,7 @@ export const generateToken = (userId,res) =>{
     res.cookie('aiinterviewjwt',token,{
         maxAge:7*24*60*60*1000,
         httpOnly:true,
-        sameSite:"strict",
+        sameSite:"none",
         secure: true,
     })
     return token;
